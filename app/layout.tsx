bb10e479cac12d1d099a16bfe8003604fd8326f2
nauroz <alinauroze@hotmail.com>
2024-09-19 15:11:11 +0500

    phantom

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,17 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import { Provider } from 'urql';
 import { SessionProvider } from 'next-auth/react';
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import Cookies from 'js-cookie';
 import { Toaster } from 'react-hot-toast';
 import Header from '@/components/Header/Header';
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from '@solana/wallet-adapter-react';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+require('@solana/wallet-adapter-react-ui/styles.css');
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,6 +24,10 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const endpoint =
+    'https://capable-rough-road.solana-devnet.discover.quiknode.pro/ef4d29ab3f286ac54013bd9e161445c749a2dd5e/';
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
   useEffect(() => {
     const token = Cookies.get('token');
     if (!token) {
@@ -32,10 +43,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Toaster />
-        <SessionProvider>
-          {<Provider value={client}>{children}</Provider>}
-        </SessionProvider>
+        <ConnectionProvider endpoint={endpoint}>
+          <WalletProvider wallets={wallets} autoConnect>
+            <WalletModalProvider>
+              <Toaster />
+              <SessionProvider>
+                {<Provider value={client}>{children}</Provider>}
+              </SessionProvider>
+            </WalletModalProvider>
+          </WalletProvider>
+        </ConnectionProvider>
       </body>
     </html>
   );
