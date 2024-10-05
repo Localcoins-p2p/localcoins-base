82e34eb0a07eb7cb5d88a0204088a1a1c9db6e1b
nauroz <alinauroze@hotmail.com>
2024-10-05 16:32:17 +0500

    sales filtering

--- server/graphql/resolvers/queries/sale.queries.ts
+++ server/graphql/resolvers/queries/sale.queries.ts
@@ -1,4 +1,5 @@
 import prisma from '@/prisma/prisma';
+import * as Prisma from '@prisma/client';
 
 export const sales = async (_: unknown, { id }: { id?: string }) => {
   const sales: any = await prisma.sale.findMany({
@@ -17,11 +18,19 @@ export const sales = async (_: unknown, { id }: { id?: string }) => {
       buyer: true,
       screenshots: true,
     },
+    orderBy: {
+      createdAt: 'desc',
+    },
   });
   if (id && sales[0].screenshots[0]) {
     sales[0].screenshots[0].method = await prisma.paymentMethod.findFirst({
       where: { id: sales[0].screenshots[0].methodId },
     });
   }
+
+  if (!id) {
+    return { sales: sales.filter((sale: Prisma.Sale) => !sale.canceledAt) };
+  }
+
   return { sales };
 };
